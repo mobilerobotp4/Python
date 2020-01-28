00d89585b049622b8484932b1a17be9ec7583236
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2020-01-27 21:10:39 -0500

    Update FrogJump.py

--- FrogJump.py
+++ FrogJump.py
@@ -1,11 +1,11 @@
-'''
+"""
 Frog Jump (LeetCode 403)
 A frog is crossing a river. The river is divided into  x  units and at each unit there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
 
 Given a list of stones' positions in sorted ascending order, determine if the forg will be able to cross the river by landing on the last stone. Initially the frog is on the first stone and assume the first jump must be  1  unit.
 
 If the frog's last jump was  k  units, then the next jump must be  k−1 ,  k  or  k+1  units. Note that the frog can only jump in the forward direction.
-'''
+"""
 def CanCrossRiver(stone_positions):
   stack = []
   stack.append((0, 1)) # (position, mean_jump_length)
@@ -27,7 +27,7 @@ def CanCrossRiver(stone_positions):
         print(stack)
 
   return False
-'''
+"""
 stone_positions = [0, 1, 3, 5, 6, 8, 12, 17]
 answer = CanCrossRiver(stone_positions)
 print(answer)
@@ -40,4 +40,4 @@ print(answer)
 [(6, 1), (8, 3)]
 [(6, 1), (12, 4)]
 True
-'''
+"""
